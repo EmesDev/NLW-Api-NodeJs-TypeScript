4a597a0f2c310633415f3a01cbe9cbd294bc9009
EmesDev <contato.emersonfranco@gmail.com>
2021-02-22 18:18:58 -0300

    1.0.1

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,13 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (req, res) =>{
+    return res.json({ massage: "Hello World!" });
+});
+
+
+app.post("/", (req, res) =>{
+    return res.json({ massage: "Os dados foram salvos com sucesso" });
+});
+
 app.listen(3333, () => (console.log("Server is Running")));
