cf1e946cfae1838ec643af315ad5dfabf9fe9813
EmesDev <contato.emersonfranco@gmail.com>
2021-02-23 21:20:17 -0300

    DataBase

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database"
+import { router } from './routes';
 
 const app = express();
 
-
-
-app.get("/", (req, res) =>{
-    return res.json({ massage: "Hello World!" });
-});
-
-
-app.post("/", (req, res) =>{
-    return res.json({ massage: "Os dados foram salvos com sucesso" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => (console.log("Server is Running")));
